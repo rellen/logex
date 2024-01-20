35fe1b9182444204b68e7985187168c496747a14
Robert Ellen <rellen@ieee.org>
2024-01-21 00:20:23 +1000

    correct evaluation sematics

--- lib/logex/compiler.ex
+++ lib/logex/compiler.ex
@@ -7,7 +7,14 @@ defmodule Logex.Compiler do
     :ladder_parser.parse(tokens)
   end
 
-  @instructions %{"xic" => {:xic, 1}, "xio" => {:xio, 1}, "ote" => {:ote, 1}, "mov" => {:mov, 2}}
+  @instructions %{
+    "xic" => {:xic, 1},
+    "xio" => {:xio, 1},
+    "ote" => {:ote, 1},
+    "otl" => {:otl, 1},
+    "otu" => {:otu, 1},
+    "mov" => {:mov, 2}
+  }
 
   def instructionize({:rung, branch}), do: {:rung, instructionize(branch)}
 
@@ -29,14 +36,12 @@ defmodule Logex.Compiler do
 
   def instructionize([]), do: []
 
-  def evaluate(_, {false, env}), do: {false, env}
-
   def evaluate({:rung, branch}, env), do: evaluate(branch, env)
 
-  def evaluate({:branches, branches}, {true, env}) do
+  def evaluate({:branches, branches}, {input, env}) do
     {outputs, new_env} =
       Enum.reduce(branches, {[], env}, fn branch, {outputs, env} ->
-        {output, new_env} = evaluate(branch, {true, env})
+        {output, new_env} = evaluate(branch, {input, env})
         {[output | outputs], new_env}
       end)
 
@@ -44,15 +49,9 @@ defmodule Logex.Compiler do
     {output, new_env}
   end
 
-  def evaluate(branch, {true, env}) when is_list(branch) do
-    Enum.reduce_while(branch, {true, env}, fn elem, {_, env} ->
-      {output, new_env} = evaluate(elem, {true, env})
-
-      if output do
-        {:cont, {true, new_env}}
-      else
-        {:halt, {false, new_env}}
-      end
+  def evaluate(branch, {input, env}) when is_list(branch) do
+    Enum.reduce(branch, {input, env}, fn elem, {input, env} ->
+      evaluate(elem, {input, env})
     end)
   end
 
@@ -60,16 +59,48 @@ defmodule Logex.Compiler do
     {Map.get(env, arg) == 1, env}
   end
 
+  def evaluate({:xic, _}, {false, env}) do
+    {false, env}
+  end
+
   def evaluate({:xio, [name: arg]}, {true, env}) do
     {Map.get(env, arg) == 0, env}
+  end
+
+  def evaluate({:xio, _}, {false, env}) do
+    {false, env}
   end
 
   def evaluate({:ote, [name: arg]}, {true, env}) do
     {true, Map.put(env, arg, 1)}
   end
 
+  def evaluate({:ote, [name: arg]}, {false, env}) do
+    {false, Map.put(env, arg, 0)}
+  end
+
+  def evaluate({:otl, [name: arg]}, {true, env}) do
+    {true, Map.put(env, arg, 1)}
+  end
+
+  def evaluate({:otl, _}, {false, env}) do
+    {false, env}
+  end
+
+  def evaluate({:otu, [name: arg]}, {true, env}) do
+    {true, Map.put(env, arg, 0)}
+  end
+
+  def evaluate({:otu, _}, {false, env}) do
+    {false, env}
+  end
+
   def evaluate({:mov, [arg1, {:name, arg2}]}, {true, env}) do
     {true, Map.put(env, arg2, get_arg(env, arg1))}
+  end
+
+  def evaluate({:mov, _}, {false, env}) do
+    {false, env}
   end
 
   defp get_arg(_env, {:lit_int, val}), do: val
--- test/logex/evaluation_test.exs
+++ test/logex/evaluation_test.exs
@@ -1,7 +1,7 @@
 defmodule Logex.EvaluationTest do
   use ExUnit.Case
 
-  test "evaluates an AST" do
+  test "evaluates an AST with OTEs" do
     ast =
       {:rung,
        [
@@ -21,7 +21,7 @@ defmodule Logex.EvaluationTest do
       "aa" => 1,
       "bb" => 2,
       "dd" => 3,
-      "xx" => 0,
+      "xx" => 1,
       "yy" => 0
     }
 
@@ -36,6 +36,45 @@ defmodule Logex.EvaluationTest do
                 "bb" => 2,
                 "dd" => 123,
                 "xx" => 0,
+                "yy" => 1
+              }}
+  end
+
+  test "evaluates an AST with OTLs and OTUs" do
+    ast =
+      {:rung,
+       [
+         branches: [
+           [{:xio, [name: "bit0"]}, {:mov, [name: "aa", name: "bb"]}],
+           [{:xic, [name: "bit0"]}, {:mov, [lit_int: 123, name: "dd"]}]
+         ],
+         branches: [
+           [{:xic, [name: "bit1"]}, {:otu, name: "xx"}],
+           [{:xio, [name: "bit1"]}, {:otl, name: "yy"}]
+         ]
+       ]}
+
+    env = %{
+      "bit0" => 1,
+      "bit1" => 0,
+      "aa" => 1,
+      "bb" => 2,
+      "dd" => 3,
+      "xx" => 1,
+      "yy" => 0
+    }
+
+    result = Logex.Compiler.evaluate(ast, {true, env})
+
+    assert result ==
+             {true,
+              %{
+                "bit0" => 1,
+                "bit1" => 0,
+                "aa" => 1,
+                "bb" => 2,
+                "dd" => 123,
+                "xx" => 1,
                 "yy" => 1
               }}
   end
